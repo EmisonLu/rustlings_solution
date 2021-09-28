f352aa8b0ce3ff52409af33c31cf1413840c3091
Emison Lu <lzh1633856298@gmail.com>
2021-09-28 15:20:41 +0800

    Upload solutions

---
+++ clippy/target/debug/deps/clippy2-f2ad007c2b4ed8b4.d
@@ -0,0 +1,7 @@
+/Users/emisonlu/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-f2ad007c2b4ed8b4.rmeta: clippy2.rs
+
+/Users/emisonlu/Desktop/rustlings/exercises/clippy/target/debug/deps/clippy2-f2ad007c2b4ed8b4.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
